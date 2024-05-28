697c6c5cb61ca1fece2722a80713489ead323c43
otso <otso@lattu.biz>
2024-05-28 22:24:51 +0300

    Included location to db schema

--- src/static/sql/init.sql
+++ src/static/sql/init.sql
@@ -1,16 +1,16 @@
-CREATE DATABASE IF NOT EXISTS julius;
+DROP TABLE IF EXISTS events;
 
 CREATE TABLE events (
-        id INT NOT NULL AUTO_INCREMENT, 
-        name varchar(255) NOT NULL, 
-        host varchar(255) NOT NULL, 
-        start TIMESTAMP NOT NULL, 
-        end TIMESTAMP NOT NULL,
-        dc varchar(255),
-        theme varchar(255),
-        price FLOAT,
-        signup varchar(255),
-        PRIMARY KEY (id)
-        );
+    id INT NOT NULL AUTO_INCREMENT,
+    name varchar(255) NOT NULL,
+    host varchar(255) NOT NULL,
+    location varchar(255),
+    start TIMESTAMP NOT NULL,
+    end TIMESTAMP NOT NULL,
+    dc varchar(255),
+    theme varchar(255),
+    price FLOAT,
+    signup varchar(255),
 
-
+    PRIMARY KEY (id)
+);
